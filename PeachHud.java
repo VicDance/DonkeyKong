eb7bdd5df0af2409f431fc742d49d4115317abec
VicDance <32700731+VicDance@users.noreply.github.com>
2019-06-11 23:58:21 +0200

    Add files via upload

---
+++ PeachHud.java
@@ -0,0 +1,44 @@
+package com.udacity.gamedev.donkeykong.overlays;
+
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.udacity.gamedev.donkeykong.util.Assets;
+import com.udacity.gamedev.donkeykong.util.Constants;
+import com.udacity.gamedev.donkeykong.util.Utils;
+
+public class PeachHud {
+
+    public final Viewport viewport;
+    final BitmapFont font;
+
+    public PeachHud() {
+        this.viewport = new ExtendViewport(Constants.HUD_VIEWPORT_SIZE, Constants.HUD_VIEWPORT_SIZE);
+        font = new BitmapFont();
+        font.getData().setScale(1);
+    }
+
+    public void render(SpriteBatch batch, int lives) {
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+
+        final TextureRegion standingRight = Assets.instance.peachAssets.walkingRight;
+        for (int i = 1; i <= lives; i++) {
+            final Vector2 drawPosition = new Vector2(
+                    viewport.getWorldWidth() - i * (Constants.HUD_MARGIN / 2 + standingRight.getRegionWidth()),
+                    viewport.getWorldHeight() - Constants.HUD_MARGIN - standingRight.getRegionHeight()
+            );
+            Utils.drawTextureRegion(
+                    batch,
+                    standingRight,
+                    drawPosition
+            );
+        }
+        batch.end();
+    }
+}
