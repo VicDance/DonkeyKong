574d1073b8ae14290f174b491602bdb9c0665b78
VicDance <32700731+VicDance@users.noreply.github.com>
2019-06-12 20:56:06 +0200

    Add files via upload

--- DonkeyScreen.java
+++ DonkeyScreen.java
@@ -1,28 +1,39 @@
 package com.udacity.gamedev.donkeykong;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.udacity.gamedev.donkeykong.overlays.GameOverOverlay;
+import com.udacity.gamedev.donkeykong.overlays.OnscreenControls;
+import com.udacity.gamedev.donkeykong.overlays.PeachHud;
+import com.udacity.gamedev.donkeykong.overlays.VictoryOverlay;
 import com.udacity.gamedev.donkeykong.util.Assets;
 import com.udacity.gamedev.donkeykong.util.ChaseCam;
 import com.udacity.gamedev.donkeykong.util.Constants;
 import com.udacity.gamedev.donkeykong.util.LevelLoader;
+import com.udacity.gamedev.donkeykong.util.Utils;
 
 public class DonkeyScreen extends ScreenAdapter {
 
+    private OnscreenControls onscreenControls;
     private Level level;
-
     private SpriteBatch batch;
-
     private ExtendViewport gameplayViewport;
-
     private ChaseCam chaseCam;
-
     private Music backgroundMusic;
+    private int contNivel = 0;
+    private PeachHud peachHud;
+    private long levelEndOverlayStartTime;
+    private GameOverOverlay gameOverOverlay;
+    private Music gameOverMusic;
+    private int contNivelVida = 0;
+    private VictoryOverlay victoryOverlay;
 
     public DonkeyScreen(){
     }
@@ -36,20 +47,38 @@ public class DonkeyScreen extends ScreenAdapter {
         batch = new SpriteBatch();
         gameplayViewport = new ExtendViewport(Constants.WORLD_SIZE*2, Constants.WORLD_SIZE*2);
 
-        level = LevelLoader.load("Level1", gameplayViewport);
-        //level = new Level(gameplayViewport);
+        peachHud = new PeachHud();
 
-        chaseCam = new ChaseCam(gameplayViewport.getCamera());
+        gameOverOverlay = new GameOverOverlay();
+
+        victoryOverlay = new VictoryOverlay();
 
         backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("raw/DonkeyKong64Music.mp3"));
         backgroundMusic.play();
 
-        //startNewLevel();
+        //onscreenControls = new OnscreenControls();
+
+        /*if (onMobile()) {
+            Gdx.input.setInputProcessor(onscreenControls);
+        }*/
+
+        startNewLevel();
+    }
+
+    private boolean onMobile() {
+        return Gdx.app.getType() == Application.ApplicationType.Android || Gdx.app.getType() == Application.ApplicationType.iOS;
     }
 
     @Override
     public void resize(int width, int height) {
         gameplayViewport.update(width, height, true);
+        peachHud.viewport.update(width, height, true);
+        gameOverOverlay.viewport.update(width, height, true);
+        victoryOverlay.viewport.update(width, height, true);
+        //chaseCam.setCamera(level.getViewport().getCamera());
+
+        //onscreenControls.viewport.update(width, height, true);
+        //onscreenControls.recalculateButtonPositions();
     }
 
     @Override
@@ -73,63 +102,87 @@ public class DonkeyScreen extends ScreenAdapter {
         batch.setProjectionMatrix(gameplayViewport.getCamera().combined);
         batch.begin();
         level.render(batch);
-
         batch.end();
+
+        //onscreenControls.render(batch);
+
+        /*if (onMobile()) {
+            onscreenControls.render(batch);
+        }*/
+
+        peachHud.render(batch, level.getPeach().getLives());
+
+        renderLevelEndOverlays(batch);
+    }
+
+    private void renderLevelEndOverlays(SpriteBatch batch){
+
+        if (level.isGameOver()) {
+
+            if (levelEndOverlayStartTime == 0) {
+                levelEndOverlayStartTime = TimeUtils.nanoTime();
+                gameOverOverlay.init();
+            }
+            gameOverOverlay.render(batch);
+            backgroundMusic.stop();
+            gameOverMusic = Gdx.audio.newMusic(Gdx.files.internal("raw/gameover.mp3"));
+            gameOverMusic.play();
+
+            if (Utils.secondsSince(levelEndOverlayStartTime) > Constants.LEVEL_END_DURATION) {
+                levelEndOverlayStartTime = 0;
+                levelFailed();
+            }
+        }else if(level.isNextLevel()){
+            levelComplete();
+        }else if(level.getPeach().isLifeLevel()){
+            getMoreLifes();
+        }else if(level.getPeach().isBackToLevel()){
+            backToLevel();
+        }
+    }
+
+    private void levelComplete(){
+        contNivel++;
+
+        if(contNivel > 2){
+            if (levelEndOverlayStartTime == 0) {
+                levelEndOverlayStartTime = TimeUtils.nanoTime();
+                victoryOverlay.init();
+            }
+            victoryOverlay.render(batch);
+        }else{
+            startNewLevel();
+
+            System.out.println("nivel:" + contNivel);
+        }
+    }
+
+    private void backToLevel(){
+        startNewLevel();
+    }
+
+    private void getMoreLifes(){
+        level = LevelLoader.load(Constants.LIFE_LEVELS[contNivelVida], gameplayViewport);
+
+        chaseCam = new ChaseCam(gameplayViewport.getCamera(), level.getPeach());
     }
 
     private void startNewLevel(){
 
-        int contNivel = 1;
-        String levelName = "";
+        level = LevelLoader.load(Constants.LEVELS[contNivel], gameplayViewport);
 
-        //while(contNivel != 3){
-            if(contNivel == 1){
-                levelName = "Level" + contNivel;
-                level = LevelLoader.load(levelName, gameplayViewport);
+        chaseCam = new ChaseCam(gameplayViewport.getCamera(), level.getPeach());
 
-                chaseCam.setCamera(level.getViewport().getCamera());
+        //onscreenControls.setPeach(level.getPeach());
 
-                resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        if(!backgroundMusic.isPlaying()){
+            backgroundMusic.play();
+        }
+    }
 
-                for(int i = 0; i < level.getKong().size; i++){
-                    if (level.getKong().get(i).getLifes() == 0){
-                        contNivel++;
-                    }
-                }
-            }/*else{
-                levelName = "Level" + contNivel;
+    public void levelFailed() {
 
-                level = LevelLoader.load(levelName, gameplayViewport);
-
-                chaseCam.setCamera(level.getViewport().getCamera());
-
-                resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-                //contNivel++;
-            }*/
-        //}
-        //levelName = "LevelOne";
-        //level = LevelLoader.load(levelName, gameplayViewport);
-
-        //chaseCam.setCamera(level.getViewport().getCamera());
-        //onscreenControls.gigaGal = level.getGigaGal();
-        //resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        //for(int i = 0; i < level.getKong().size; i++) {
-            /*if (level.getKong().get(i).getLifes() == 0){
-                levelName = "Level" + contNivel;
-                level = LevelLoader.load(levelName, gameplayViewport);
-
-                chaseCam.setCamera(level.getViewport().getCamera());
-                //onscreenControls.gigaGal = level.getGigaGal();
-                resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            }*//*else{
-                levelName = "LevelOne";
-                level = LevelLoader.load(levelName, gameplayViewport);
-
-                chaseCam.setCamera(level.getViewport().getCamera());
-                //onscreenControls.gigaGal = level.getGigaGal();
-                resize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            }*/
-        //}
+        gameOverMusic.stop();
+        startNewLevel();
     }
 }
